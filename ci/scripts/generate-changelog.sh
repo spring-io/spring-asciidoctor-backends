7b8dd984362c4f01b84113df54a751f1d2bcefe3
Phillip Webb <pwebb@vmware.com>
2021-03-18 19:20:02 -0700

    Update generate-changelog CI task

--- ci/scripts/generate-changelog.sh
+++ ci/scripts/generate-changelog.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 set -e
 
-version=$( cat built-artifact/version )
+CONFIG_DIR=git-repo/ci/config
+version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
 
 java -jar /github-changelog-generator.jar \
   --changelog.repository=spring-io/asciidoctor-spring-backend \
