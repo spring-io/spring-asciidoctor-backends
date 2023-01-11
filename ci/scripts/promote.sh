891aa75af393ec73a501ff54e72a4cdad16c1152
Andy Wilkinson <wilkinsona@vmware.com>
2023-01-11 14:31:43 +0000

    Publish releases to Maven Central
    Closes gh-72

--- ci/scripts/promote.sh
+++ ci/scripts/promote.sh
@@ -1,35 +1,17 @@
 #!/bin/bash
-set -e
 
-source $(dirname $0)/common.sh
+CONFIG_DIR=git-repo/ci/config
 
-buildName=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.name' )
-buildNumber=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.number' )
 version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
+export BUILD_INFO_LOCATION=$(pwd)/artifactory-repo/build-info.json
 
+java -jar /concourse-release-scripts.jar \
+  --spring.config.location=${CONFIG_DIR}/release-scripts.yml \
+  publishToCentral $RELEASE_TYPE $BUILD_INFO_LOCATION artifactory-repo || { exit 1; }
 
-if [[ $RELEASE_TYPE = "M" ]]; then
-	targetRepo="libs-milestone-local"
-elif [[ $RELEASE_TYPE = "RC" ]]; then
-	targetRepo="libs-milestone-local"
-elif [[ $RELEASE_TYPE = "RELEASE" ]]; then
-	targetRepo="libs-release-local"
-else
-	echo "Unknown release type $RELEASE_TYPE" >&2; exit 1;
-fi
-
-echo "Promoting ${buildName}/${buildNumber} to ${targetRepo}"
-
-curl \
-	-s \
-	--connect-timeout 240 \
-	--max-time 900 \
-	-u ${ARTIFACTORY_USERNAME}:${ARTIFACTORY_PASSWORD} \
-	-H "Content-type:application/json" \
-	-d "{\"status\": \"staged\", \"sourceRepo\": \"libs-staging-local\", \"targetRepo\": \"${targetRepo}\"}"  \
-	-f \
-	-X \
-	POST "${ARTIFACTORY_SERVER}/api/build/promote/${buildName}/${buildNumber}" > /dev/null || { echo "Failed to promote" >&2; exit 1; }
+java -jar /concourse-release-scripts.jar \
+  --spring.config.location=${CONFIG_DIR}/release-scripts.yml \
+  promote $RELEASE_TYPE $BUILD_INFO_LOCATION || { exit 1; }
 
 echo "Promotion complete"
 echo $version > version/version
